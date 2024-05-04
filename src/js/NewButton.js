874f25a979e6ce6c35cedf6ff3691deeee6312df
GuilCube <qwparty10@gmail.com>
2024-05-04 14:48:59 +0300

    temp commit

--- src/js/NewButton.js
+++ src/js/NewButton.js
@@ -26,3 +26,19 @@ leadForm.addEventListener('submit', (event) => {
     // You can handle form submission logic here (e.g., send data to server)
     console.log('Form submitted!');
 });
+
+function setTextareaHeightToLineHeight(textarea) {
+    var lineHeight = parseFloat(window.getComputedStyle(textarea).lineHeight); // Get line height
+    textarea.style.height = lineHeight + 7 + 'px'; // Set textarea height to line height
+}
+
+// Apply behavior to all textareas
+document.addEventListener('DOMContentLoaded', function() {
+    var textareas = document.querySelectorAll('textarea'); // Select all textareas
+    textareas.forEach(function(textarea) {
+        setTextareaHeightToLineHeight(textarea); // Apply function to each textarea
+        textarea.addEventListener('input', function() {
+            setTextareaHeightToLineHeight(textarea); // Adjust height on input
+        });
+    });
+});
