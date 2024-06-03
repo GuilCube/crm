7e9e1d7bce2840e1253777ac7011eb303068a3f1
GuilCube <qwparty10@gmail.com>
2024-06-03 15:16:52 +0300

    added inbound front

--- js/Depot.js
+++ js/Depot.js
@@ -39,6 +39,7 @@ function buttons() {
 
     NewGoodForm();
     NewArchiveForm();
+    NewInboundForm();
 
 
     //New good form
@@ -274,6 +275,184 @@ function buttons() {
                 }
             });
         });
+    }
+
+    function NewInboundForm() {
+         //Modal form
+    const modalContainer = $('<div>').attr('id', 'inbound').addClass('modal-container');
+    const modalContent = $('<div>').addClass('modal-content');
+    const modalTitle = $('<h3>').css('text-align', 'center').text('Нове надходження...');
+    const leadForm = $('<form>').attr('id', 'inboundForm').addClass('leadForm');
+
+    function createGoodsLineB($goodContainerDIV) {
+        const labelCount = $goodContainerDIV.find('input#goods').length;
+        console.log(labelCount);
+        let goodNum;
+        if (labelCount > 0) {
+            console.log($('input#goods:first').parent().parent().find('label').text('Товар 1'));
+            goodNum = labelCount + 1;
+        }
+        else
+            goodNum = '';
+        const $removeRowBtn = $('<span>').addClass('btn remove').text('X').css('display','block')
+
+        $removeRowBtn.click(function (e) {
+            e.preventDefault();
+            //if($removeRowBnt.parent().parent())
+            if ($goodContainerDIV.find('input#goods').length > 1) {
+                $removeRowBtn.closest('.modal-line').slideDown(200)
+                $removeRowBtn.closest('.modal-line').remove()
+                console.log($('input#goods').parent().parent().find('label')); 
+                $goodContainerDIV.find('label').each(function(index) {
+                    $(this).text('Товар ' + (index + 1));
+                });
+                if($goodContainerDIV.find('label').length==1)
+                    $goodContainerDIV.find('label').text('Товар');
+            }
+            else
+            {
+                $removeRowBtn
+                    .siblings('input#goods').val('').prop('placeholder', 'Назва товару...')
+                    .siblings('input.qty').val('');
+                    
+            }
+        });
+
+        const $line = $('<div>').addClass('modal-line')
+        const $goodContainer = $('<div>').addClass('good-container');
+        const $label = $('<label>').attr('for', 'text').text('Товар ' + goodNum)
+        const $qty = $('<input>').addClass('qty editable')
+        const $input = $('<input>').attr({ type: 'text', id: 'goods', name: 'goods', placeholder: 'Назва товару...' });
+        $goodContainer.append($input, $qty, $removeRowBtn)
+        $line.append($label, $goodContainer)
+        return $line
+    }
+
+    ///const $goodsModal=createModalLine('Товари', 'text', 'leadPhone', 'Введіть номер телефону...').append($qty);
+    const $createLine = $('<span>').addClass('btn action add-line inform').text('+')
+    //$goodContainer.append($goodsModal,$qty)
+    //const dropdownOptions = ['Оформлено', 'Комплектується', 'Відправлено'];
+    const $goodContainerDIV = $('<div>').addClass('good-section')
+    $goodContainerDIV.append(createGoodsLineB($goodContainerDIV),$createLine)
+
+    leadForm.append(
+        $goodContainerDIV,        
+        createModalLine('Відправник', 'text', 'l_name', 'Ведіть дані відправника...'),
+    );
+
+    $(document).ready(function () {
+        $(document).on('click', '.dropbtn', function () {
+            $('.dropdown-content').removeClass('show');
+            $(this).siblings('.dropdown-content').toggleClass('show');
+        });
+        $createLine.click(function (e) {
+            e.preventDefault();
+            //console.log($(this));
+            if ($(this).parent().find('input#goods:last').val() != 0) {
+                $goodContainerDIV.find('label:first').text("Товар 1")
+                const $line = createGoodsLineB($goodContainerDIV).hide();
+                $(this).before($line)
+                $line.slideDown(200);
+            }
+        });
+
+
+        $(document).on('click', '.dropdown-content a', function (e) {
+            e.preventDefault();
+            var value = $(this).data('value');
+            //console.log(value);
+            $(this).closest('.input-container').find('input').val(value);
+            $(this).parent().removeClass('show');
+        });
+
+
+        $(window).click(function (e) {
+            if (!$(e.target).closest('.input-container').length) {
+                $('.dropdown-content').removeClass('show');
+            }
+        });
+    });
+
+
+    const leadCommentLine = $('<div>').addClass('modal-line')
+        .append($('<label>').attr('for', 'leadComment').text('Коментар'))
+        .append($('<textarea>').attr({ id: 'leadComment', name: 'leadComment', placeholder: 'Тут може бути коментар до ліда...' }).addClass('comment'));
+
+    const $submitButton = $('<button>').attr('type', 'submit').addClass('btn action').text('Створити')
+
+    const $buttonCancel =$('<button>').attr('id', 'closeModal').addClass('close btn back').text('Назад')
+    const buttonLine = $('<div>').addClass('modal-line-buttons')
+        .append($buttonCancel,$submitButton);
+
+    leadForm.append(leadCommentLine);
+    modalContent.append(modalTitle, leadForm, buttonLine);
+    modalContainer.append(modalContent);
+    $('main').append(modalContainer);
+
+    NewInbound.on('click', function () {
+        modalContainer.fadeIn(200);
+    });
+    $buttonCancel.on('click', function () {
+
+        modalContainer.fadeOut(100);
+        setTimeout(() => {
+            $('input#goods').parent().parent().not(':first').remove();
+            $('input#goods').parent().parent().find('label').text('Товар')
+            $('input#goods').parent().parent().find('input').val('')
+        }, 100)
+
+    });
+
+
+    modalContainer.on('click', function (event) {
+        if ($(event.target).is(modalContainer)) {
+            modalContainer.fadeOut(200);
+        }
+    });
+
+    // Handle form submission
+    $submitButton.on('click', function (event) {
+        event.preventDefault();
+        const $parentContainer = $(this).closest('.modal-content');
+        console.log($parentContainer);
+        console.log($(this));
+        const goods = [];
+        console.log($('.good-container'));
+        $('.good-container').each(function() {
+            
+            const g_name = $(this).find('input:first-child').val();
+            const g_quantity = $(this).find('input.qty').val();
+            console.log(g_name);
+            console.log(g_quantity);
+            if (g_name && g_quantity) {
+                goods.push({ g_name: g_name, g_quantity: g_quantity });
+            }
+        });
+        
+
+        const dataPOST = {
+            leadName:  $parentContainer.find('input#l_name').val(),
+            adress:  $parentContainer.find('input#adress').val(),
+            o_comment:  $parentContainer.find('textarea#leadComment').val(),
+            goods: goods
+        };
+        console.log(goods);
+        console.log(dataPOST);
+        $.ajax({
+            type: 'POST',
+            url: 'submitOrder.php',
+            data: JSON.stringify(dataPOST),
+            contentType: 'application/json; charset=utf-8',
+            success: function (response) {
+                showAlert('Замовнстворено успішно', 3000);
+                modalContainer.fadeOut(200);
+                console.log(response);
+            },
+            error: function () {
+                showAlert('Виникла помилка', 3000);
+            }
+        });
+    });
     }
 
 }
